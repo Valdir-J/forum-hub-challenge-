fca23c77f37e5d5ec672660ee4a91a816236d075
Valdir-J <algumascoisasaleatorias@gmail.com>
2026-01-12 15:08:53 -0300

    Initial commit

---
+++ src/main/java/com/example/api/forumhub/ForumHubApplication.java
@@ -0,0 +1,19 @@
+package com.example.api.forumhub;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumHubApplication {
+
+    public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(entry ->
+                System.setProperty(entry.getKey(), entry.getValue())
+        );
+
+        SpringApplication.run(ForumHubApplication.class, args);
+    }
+
+}
