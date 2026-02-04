da090468ff0dd18fe77342cc00641e1bf1f16f7c
Valdir-J <algumascoisasaleatorias@gmail.com>
2026-02-04 14:17:05 -0300

    feat: retorna erro 400 para validações inválidas
    As resposta agora retornam um json contendo o erro e uma mensagem.

--- src/main/java/com/example/api/forumhub/infra/exception/TratadorDeErros.java
+++ src/main/java/com/example/api/forumhub/infra/exception/TratadorDeErros.java
@@ -1,7 +1,10 @@
 package com.example.api.forumhub.infra.exception;
 
 import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
@@ -11,14 +14,32 @@ public class TratadorDeErros {
 
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity tratarErro404() {
-        return ResponseEntity.notFound().build();
+        var erro = new DadosErro("Not Found", "Recurso não encontrado");
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
     }
 
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     public ResponseEntity tratarErro400(MethodArgumentTypeMismatchException ex) {
         if (ex.getParameter().getParameterName().equals("id")) {
-            return ResponseEntity.badRequest().body("Id inválido");
+            var erro = new DadosErro("Bad Request", "Id inválido");
+            return ResponseEntity.badRequest().body(erro);
         }
         return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarErro400(MethodArgumentNotValidException ex) {
+        var erros = ex.getFieldErrors().stream()
+                .map(DadosErroValidacao::new)
+                .toList();
+        return ResponseEntity.badRequest().body(erros);
+    }
+
+    private record DadosErro(String error, String message) {}
+
+    private record DadosErroValidacao(String field, String message) {
+        public DadosErroValidacao(FieldError erro) {
+            this(erro.getField(), erro.getDefaultMessage());
+        }
     }
 }
