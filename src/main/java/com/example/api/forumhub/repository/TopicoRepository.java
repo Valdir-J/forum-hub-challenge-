8e2f76f34ff813ad2687d6a5d25da9daed17718c
Valdir-J <algumascoisasaleatorias@gmail.com>
2026-02-13 20:58:21 -0300

    style: formatação do código

--- src/main/java/com/example/api/forumhub/repository/TopicoRepository.java
+++ src/main/java/com/example/api/forumhub/repository/TopicoRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
-    boolean existsByTituloAndMensagem(String titulo,String mensagem);
+    boolean existsByTituloAndMensagem(String titulo, String mensagem);
 
     Page<Topico> findAllByAutorAtivoTrue(Pageable paginacao);
 
